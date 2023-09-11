1cbe79f59affd0cfa0e44ed8ea40ca4e51b387d3
Jovan Maksic <jovan.maksic@clovertech.com>
2023-09-11 11:56:49 +0200

    initial push

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+export default function AboutPage() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>This is app to find the latests DJ and other usical events</p>
+      <p>Version: 1.0.0</p>
+    </div>
+  );
+}
